8e205f1b89748b7b97021258bf4927fc0cf52b4a
miantsaT <miantsarakotoniaina+github@gmail.com>
2023-04-19 22:39:56 +0200

    portfolio media

--- js/main.js
+++ js/main.js
@@ -12,3 +12,19 @@ wrapper.addEventListener('scroll', () => {
     header.classList.remove('header_bg');
   }
 });
+
+function toggleMute() {
+  var video = document.getElementById("sncf-video");
+  var sound = document.getElementById("sncf-sound");
+  
+  if (video.muted) {
+    sound.innerHTML = '<i class="fas fa-volume-mute px-1"></i> Sound Off';
+  } else {
+    sound.innerHTML = '<i class="fas fa-volume-up px-1"></i> Sound On';
+  }
+  video.pause();
+  video.currentTime = '0';
+  video.muted = !video.muted;
+  video.play();
+};
+
